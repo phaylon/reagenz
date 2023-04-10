f3a0c4d92c159991e112264e226663798ad463f8
Robert Sedlacek <rs@474.at>
2023-04-10 15:26:52 +0200

    script source helpers

--- src/tree/script.rs
+++ src/tree/script.rs
@@ -14,3 +14,16 @@ pub enum ScriptSource {
     Path { path: Arc<Path> },
     Str { content: Box<str>, name: Arc<str> },
 }
+
+impl ScriptSource {
+    pub fn from_path<P>(path: P) -> Self
+    where
+        P: AsRef<Path>,
+    {
+        Self::Path { path: path.as_ref().into() }
+    }
+
+    pub fn from_named(name: &str, content: Box<str>) -> Self {
+        Self::Str { name: name.into(), content }
+    }
+}
